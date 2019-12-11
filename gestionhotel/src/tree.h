183dc23264cc783f5a9e39b74612b47efcec5f8f
latyfa2019 <latifa.sassi1@esprit.tn>
2019-12-11 12:12:01 -0800

    ajout gestionhotel

---
+++ gestionhotel/src/tree.h
@@ -0,0 +1,7 @@
+
+
+void Hoteltree(GtkWidget* treeview1,char*l);
+void Hoteltree2(GtkWidget* treeview1,char*l);//affichage du 2emme treeView (page chercher)
+int ChercherHotelTree(GtkWidget* treeview1,char*l,char*type);
+
+
